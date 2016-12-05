fdcc96c6211f8a1f252c18d728bc0bc6bd9bd791
drongo-spindletop <aponomarev@spindletop.software>
2016-12-05 16:52:28 +0600

    444

--- services/config.rb
+++ services/config.rb
@@ -31,7 +31,7 @@ coreo_aws_vpc_vpc "${VPC_NAME}" do
    action :find
    cidr "${VPC_CIDR}"
    #internet_gateway true
-   name "${VPC_NAME}"
+   Name "${VPC_NAME}"
 end
 
 coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}" do
